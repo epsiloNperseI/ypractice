8d850c75054c797299096762cb973d7e833ea32f
Epsilon <dzhulakyant@gmail.com>
2026-04-27 12:46:44 +0300

    MaxNumberKSumPairs

---
+++ src/main/java/ypractice/patterns/two_pointers/FindAllAnagramsInString.java
@@ -0,0 +1,13 @@
+package ypractice.patterns.two_pointers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllAnagramsInString {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+
+        return result;
+    }
+}
